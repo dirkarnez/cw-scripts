9c6cfe22297722035803fc58c5f37f3a915d349e
Dirk Arnez <smalldirkalex@gmail.com>
2023-01-02 19:59:38 +0800

    - update ffmpeg/trim.bat

--- ffmpeg/trim.bat
+++ ffmpeg/trim.bat
@@ -3,7 +3,7 @@ set /p url="Enter input file name / full path: "
 set /p filename="Enter output file name / full path: "
 set /p start="Start: "
 set /p end="End: "
-ffmpeg -i "%url%" -ss %start% -to %end% "%filename%"
+ffmpeg -i "%url%" -ss %start% -to %end% -c copy "%filename%"
 pause
 
  
